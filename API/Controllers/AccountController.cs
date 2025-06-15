f333579ffb8d6536f171119ebdc2acda2038ceff
jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2025-06-14 21:00:59 -0600

    changes in db, and entities. user register

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.DTO;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController(DataContext context) : BaseApiController
+    {
+        [HttpPost("register")]// account/register
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
+        {
+            if (await UserExists(registerDTO.Username)) return BadRequest("Username already taken.");
+
+            using var hmac = new HMACSHA3_512();
+            var user = new AppUser
+            {
+                UserName = registerDTO.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+        }
+    }
+}
