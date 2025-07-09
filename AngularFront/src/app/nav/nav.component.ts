f9a7f96c18ab9b4d923ccc39376c8ec22c2ce34f
jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2025-07-08 22:34:42 -0600

    addded persistent loggin using signals

--- AngularFront/src/app/nav/nav.component.ts
+++ AngularFront/src/app/nav/nav.component.ts
@@ -1,31 +1,33 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { NgIf } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, BsDropdownModule],
+
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-  private accountService = inject(AccountService);
-  isLogged = false;
+  public accountService = inject(AccountService);
+  // isLogged = false;
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.isLogged = true;
+        // this.isLogged = true;
       },
       error: error => console.log(error),
     })
   }
 
   logOut() {
-    this.isLogged = false;
+    // this.isLogged = false;
+    this.accountService.logout();
   }
 }
